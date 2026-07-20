1377b454e30dda87ee31e6397a0c7dd2d1e0dd1b
BHARATH-942 <yamjalabharathreddy942@gmail.com>
2026-07-20 15:37:31 +0530

    Configure Vercel deployment

--- app.js
+++ app.js
@@ -138,6 +138,10 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+}
+
+module.exports = app;
